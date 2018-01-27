2b190ef29170afdcd9f4f8c2f06b85696ad941d6
Christopher P. Dundon <chris.dundon@gmail.com>
2018-01-27 18:56:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWisper(string) {
+  console.log(string.toLowerCase())
+}
